26cc7289e163b664dc999a0f23120ca422fd7a01
尹成林 <sdyinruichao@163.com>
2018-09-16 16:03:53 +0800

    1

---
+++ 集合子集/src/集合子集/first.java
@@ -0,0 +1,21 @@
+package 集合子集;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void shu(int a) {
+		for(int ii = 0;ii<a;ii++) {
+			
+		}
+	}
+	public void shushu(int a) {
+		for(int i = 0;i<2;i++) {
+			System.out.print(i);
+		}
+	}
+	
+}
